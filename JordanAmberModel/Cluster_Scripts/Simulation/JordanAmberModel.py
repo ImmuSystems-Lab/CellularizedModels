875bad52097fa8e87423309823d459bcfca8204f
joaponte <josua.aponte@gmail.com>
2021-07-11 15:35:14 -0400

    Deleted Directories

--- JordanAmberModel/Cluster_Scripts/Simulation/JordanAmberModel.py
+++
@@ -1,9 +0,0 @@
-from cc3d import CompuCellSetup
-
-from JordanAmberModelSteppables import ModelSteppable
-CompuCellSetup.register_steppable(steppable=ModelSteppable(frequency=1))
-
-from JordanAmberModelSteppables import PlaqueAssaySteppable
-CompuCellSetup.register_steppable(steppable=PlaqueAssaySteppable(frequency=1))
-
-CompuCellSetup.run()
