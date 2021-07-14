1cc863d8f0910f009ac75cc08005dbaa3822a4e2
joaponte <josua.aponte@gmail.com>
2021-07-14 13:57:16 -0400

    Refactored file names, edited readme

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Multicellular Spatial Model of IFN Response to Viral Infection
 
+This respository contains the code to implement the model and results presented in:
+
+The directory Simulation_Code contains the code to simulate the model using CompuCell3D (CC3D). Users need a local 
+version of CC3D to execute the code. Inside the directory, users can find the main simulation file: IFNModel.cc3d. 
+To execute the base 
+
--- Simulation_Code/Simulation/IFNModel.py
+++ Simulation_Code/Simulation/IFNModel.py
@@ -1,16 +1,15 @@
-
 from cc3d import CompuCellSetup
         
-from JordanAmberModelSteppables import ODEModelSteppable
+from IFNModelSteppables import ODEModelSteppable
 CompuCellSetup.register_steppable(steppable=ODEModelSteppable(frequency=1))
 
-from JordanAmberModelSteppables import CellularModelSteppable
+from IFNModelSteppables import CellularModelSteppable
 CompuCellSetup.register_steppable(steppable=CellularModelSteppable(frequency=1))
 
-from JordanAmberModelSteppables import PlaqueAssaySteppable
+from IFNModelSteppables import PlaqueAssaySteppable
 CompuCellSetup.register_steppable(steppable=PlaqueAssaySteppable(frequency=1))
 
-from JordanAmberModelSteppables import OutputSteppable
+from IFNModelSteppables import OutputSteppable
 CompuCellSetup.register_steppable(steppable=OutputSteppable(frequency=1))
 
 CompuCellSetup.run()
